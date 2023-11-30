c9e310c9f7e66323af3927d659474c4c1ca1b603
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-11-30 19:19:26 -0300

    add styles

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
 import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 
 export default function App() {
+  const [numberTime, setNumberTime] = useState(0);
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" backgroundColor="gray" />
       <Image source={require("./src/images/cronometro.png")} />
-      <Text styles={styles.timer}>0.0</Text>
+      <Text style={styles.timer}>{numberTime.toFixed(1)}</Text>
       <View style={styles.buttonArea}>
         <TouchableOpacity style={styles.timerButton}>
           <Text style={styles.textTimerButton}>Iniciar</Text>
@@ -23,5 +26,33 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#222",
+  },
+  timer: {
+    fontSize: 65,
+    color: "#fff",
+    marginTop: -160,
+    fontWeight: "bold",
+  },
+  buttonArea: {
+    flexDirection: "row",
+    marginTop: 100,
+    height: 40,
+  },
+  timerButton: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    height: 40,
+    margin: 17,
+    borderRadius: 9,
+    backgroundColor: "#fff",
+  },
+  textTimerButton: {
+    color: "#222",
+    fontWeight: "bold",
+    fontSize: 18,
+    textTransform: "uppercase",
   },
 });
